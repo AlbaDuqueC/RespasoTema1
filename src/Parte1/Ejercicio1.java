347d8d11401d210d1bfc76f5c8fb5c968af8ffeb
albad <albad@192.168.1.29>
2024-10-02 21:55:59 +0200

    1.1

--- src/Parte1/Ejercicio1.java
+++ src/Parte1/Ejercicio1.java
@@ -1,9 +1,27 @@
 package Parte1;
 
+import java.util.Scanner;
+
 public class Ejercicio1 {
 
 	public static void main(String[] args) {
+		//Variabales 
+		double num;
 		
+		//creamos scanner
+		Scanner sc= new Scanner(System.in) ;
+		
+		//Pide un numero
+		System.out.println("Inserta un numero");
+		
+		//Introduce un numero
+		num= sc.nextDouble();
+		
+		//Imprime el numero insertado
+		System.out.println(num);
+		
+		//cierre del scanner
+		sc.close();
 
 	}
 
