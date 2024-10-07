57cfdaf013d60cb91eeb34d7765aae05a42b6638
albad <albad@192.168.1.33>
2024-10-07 19:40:51 +0200

    8

--- src/Parte2/Ejercicio8.java
+++ src/Parte2/Ejercicio8.java
@@ -1,9 +1,30 @@
 package Parte2;
 
+import java.util.Scanner;
+
 public class Ejercicio8 {
 
 	public static void main(String[] args) {
-		// 
+		// Valores
+		double metros;
+		int cm;
+		
+		//Constantes
+		final int CONVERSION= 100;
+		
+		//Scanner
+		Scanner sc= new Scanner(System.in);
+		
+		System.out.println("Introduzca la medida en metros: ");
+		
+		metros= sc.nextDouble();
+		
+		cm= (int) (metros*CONVERSION);
+		
+		System.out.println(cm);
+		
+		//Cierre del Scanner
+		sc.close();
 
 	}
 
